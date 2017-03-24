c0dcc70fa3a3d0546f6d642c596fcfec831825c9
Max Skala <max.skala@odysys.com>
2017-03-24 12:07:56 -0400

    Fix SignupLoginForm tests

--- client/src/tests/SignupLoginForm.test.js
+++ client/src/tests/SignupLoginForm.test.js
@@ -1,43 +1,42 @@
 import React from 'react';
 
-import { wrapAndSetState, setStateAndRender } from './utils';
+import { wrap, wrapAndRender } from './utils';
 import SignupLoginForm from '../components/SignupLoginForm';
 
 const EMPTY_FUNCTION = () => {};
 const EMPTY_PROPS = {
-  onLogin: EMPTY_FUNCTION,
-  onSignup: EMPTY_FUNCTION,
+  onSubmit: EMPTY_FUNCTION,
   onReceiveUser: EMPTY_FUNCTION,
+  changeAction: EMPTY_FUNCTION,
   errors: [],
 };
+const LOGIN_PROPS = { action: 'login', ...EMPTY_PROPS };
+const SIGNUP_PROPS = { action: 'signup', ...EMPTY_PROPS };
 const ERRORS = ['This is an error', 'As is this'];
 const ERRORS_LENGTH = ERRORS.length;
 
 describe('<SignupLoginForm />', () => {
-  it('shows login header when showLogin === true', () => {
-    const signupLoginForm = setStateAndRender(
-      <SignupLoginForm {...EMPTY_PROPS} />,
-      { showLogin: true }
+  it('shows login header when props.action === "login"', () => {
+    const signupLoginForm = wrapAndRender(
+      <SignupLoginForm { ...LOGIN_PROPS } />
     );
     const h3s = signupLoginForm.find('h3');
     expect(h3s.length).toBe(1);
     expect(h3s.text()).toBe('Login');
   });
 
-  it('shows signup toggle when showLogin === true', () => {
-    const signupLoginForm = setStateAndRender(
-      <SignupLoginForm {...EMPTY_PROPS} />,
-      { showLogin: true }
+  it('shows signup toggle when props.action === "login"', () => {
+    const signupLoginForm = wrapAndRender(
+      <SignupLoginForm { ...LOGIN_PROPS } />
     );
     const anchors = signupLoginForm.find('a');
     expect(anchors.length).toBe(1);
     expect(anchors.text()).toBe('Signup');
   });
 
-  it('shows signup header when showLogin === false', () => {
-    const signupLoginForm = setStateAndRender(
-      <SignupLoginForm {...EMPTY_PROPS} />,
-      { showLogin: false }
+  it('shows signup header when props.action === "signup"', () => {
+    const signupLoginForm = wrapAndRender(
+      <SignupLoginForm { ...SIGNUP_PROPS } />
     );
     const h3s = signupLoginForm.find('h3');
     expect(h3s.length).toBe(1);
@@ -45,9 +44,8 @@ describe('<SignupLoginForm />', () => {
   });
 
   it('shows login toggle when showLogin === false', () => {
-    const signupLoginForm = setStateAndRender(
-      <SignupLoginForm {...EMPTY_PROPS} />,
-      { showLogin: false }
+    const signupLoginForm = wrapAndRender(
+      <SignupLoginForm { ...SIGNUP_PROPS } />
     );
     const anchors = signupLoginForm.find('a');
     expect(anchors.length).toBe(1);
@@ -55,9 +53,8 @@ describe('<SignupLoginForm />', () => {
   });
 
   it('does not render .errors-container when no errors', () => {
-    const signupLoginForm = setStateAndRender(
-      <SignupLoginForm {...EMPTY_PROPS} />,
-      {}
+    const signupLoginForm = wrapAndRender(
+      <SignupLoginForm { ...SIGNUP_PROPS } />
     );
     expect(signupLoginForm.find('.errors-container').length).toBe(0);
   });
@@ -65,24 +62,22 @@ describe('<SignupLoginForm />', () => {
   it('renders .errors-container when there are errors', () => {
     const errors = ['This is an error'];
     const props = {
-      ...EMPTY_PROPS,
+      ...LOGIN_PROPS,
       errors
     };
-    const signupLoginForm = setStateAndRender(
-      <SignupLoginForm {...props} />,
-      {}
+    const signupLoginForm = wrapAndRender(
+      <SignupLoginForm {...props} />
     );
     expect(signupLoginForm.find('.errors-container').length).toBe(1);
   });
 
   it('renders multiple errors if there are more than one', () => {
     const props = {
-      ...EMPTY_PROPS,
+      ...LOGIN_PROPS,
       errors: ERRORS
     };
-    const signupLoginForm = setStateAndRender(
-      <SignupLoginForm {...props} />,
-      {}
+    const signupLoginForm = wrapAndRender(
+      <SignupLoginForm {...props} />
     );
     expect(signupLoginForm.find('.errors-container .error').length)
       .toBe(ERRORS_LENGTH);
@@ -90,12 +85,11 @@ describe('<SignupLoginForm />', () => {
 
   it('renders error text correctly', () => {
     const props = {
-      ...EMPTY_PROPS,
+      ...LOGIN_PROPS,
       errors: ERRORS
     };
-    const signupLoginForm = wrapAndSetState(
-      <SignupLoginForm {...props} />,
-      {}
+    const signupLoginForm = wrap(
+      <SignupLoginForm {...props} />
     );
     const errors = signupLoginForm.find('.errors-container .error');
     errors.forEach((error, i) => {
