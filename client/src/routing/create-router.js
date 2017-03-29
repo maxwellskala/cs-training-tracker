8aa45473e6f5c0df23a293966937482871b45967
Max Skala <max.skala@odysys.com>
2017-03-29 16:15:51 -0400

    Make trailing slashes optional for routing

--- client/src/routing/create-router.js
+++ client/src/routing/create-router.js
@@ -2,10 +2,16 @@ import createRouter from 'router5';
 import loggerPlugin from 'router5/plugins/logger';
 import listenersPlugin from 'router5/plugins/listeners';
 import browserPlugin from 'router5/plugins/browser';
+import * as RouteNames from '../constants/RouteNames';
 import routes from './routes';
 
+const options = {
+  defaultRoute: RouteNames.ROOT,
+  trailingSlash: true
+};
+
 function configureRouter() {
-  return createRouter(routes, { defaultRoute: 'loading' })
+  return createRouter(routes, options)
     .usePlugin(loggerPlugin)
     .usePlugin(browserPlugin())
     .usePlugin(listenersPlugin());
