60d5e588b31866b4373df589ce25624235078c17
Max Skala <max.skala@odysys.com>
2017-03-31 14:23:39 -0400

    Wire AimContainer to handle fetching and rendering of configs

--- client/src/containers/AimContainer.js
+++ client/src/containers/AimContainer.js
@@ -1,9 +1,10 @@
-import React, { createElement } from 'react';
+import React, { Component, createElement } from 'react';
 import { routeNode } from 'react-router5';
 import AimNav from '../components/AimNav';
 import AddSession from '../components/AddSession';
 import History from '../components/History';
 import AddConfig from '../components/AddConfig';
+import Loading from '../components/Loading';
 import * as RouteNames from '../constants/RouteNames';
 
 const COMPONENTS = {
@@ -13,24 +14,40 @@ const COMPONENTS = {
   [RouteNames.AIM_ADD_CONFIG]: AddConfig
 };
 
-const AimContainer = ({ user, onLogout, route, router }) => {
-  const body = COMPONENTS[route.name];
-  return (
-    <div className='Aim'>
-      <aside>
-        <AimNav route={route} router={router} />
-      </aside>
-      <main>
-        {createElement(body)}
-        <button onClick={onLogout}>Log out</button>
-      </main>
-    </div>
-  );
-}
+class AimContainer extends Component {
+  componentDidMount() {
+    this.props.fetchConfigs();
+  };
+
+  render() {
+    const {
+      route,
+      router,
+      configs,
+      onLogout
+    } = this.props;
+    const body = configs === null
+      ? Loading
+      : COMPONENTS[route.name];
+    return (
+      <div className='Aim'>
+        <aside>
+          <AimNav route={route} router={router} />
+        </aside>
+        <main>
+          {createElement(body, { configs })}
+          <button onClick={onLogout}>Log out</button>
+        </main>
+      </div>
+    );
+  };
+};
 
 AimContainer.propTypes = {
   user: React.PropTypes.object.isRequired,
   onLogout: React.PropTypes.func.isRequired,
+  fetchConfigs: React.PropTypes.func.isRequired,
+  configs: React.PropTypes.array,
   route: React.PropTypes.object.isRequired,
   router: React.PropTypes.object.isRequired
 };
